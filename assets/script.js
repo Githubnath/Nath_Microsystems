9da50e5b4e42da2e2c2c9fe7e77db69e5815fea5
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2025-03-28 23:44:54 +0000

    web

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
 // Content for different pages
 const pages = {
     home: `
-        <h1>Welcome to Nath Microsystems, your ultimate Web development company in Nigeria</h1>
+        <h1>Welcome To Nath Microsystems</h1>
+	<h2>Your Ultimate Web Development Company In Nigeria</h2>
         <img src="images/home.jpg" alt="Home">
-        <p>We provide top-notch services to our clients worldwide.</p>
+        <p>We build future-proof and innovative solutions to foster your startup’s growth.Speed up your business growth with our full range of customer-centric, high-performance web development services in the Nigeria.
+	What makes us a leading web development company in the Nigeria? It is a combination of technical expertise, hands-on experience, cutting-edge technology, and diverse web development services.
+We thrive in the highly competitive digital landscape by following transparent, DevOps-based, client-centric approaches to deliver high-performing quality web apps and solutions. Partner with us to fulfill your digital aspirations and make a significant impact in the Nigeria’s digital arena.
+</p>
+
+<h2>Innovators, Not Just Problem Solvers</h2>
+
+<p> We’re more than just a software development company — we’re your digital product development and system integration partner.
+Our focus is to create digital products that generates lasting value for your business. Because we don’t just write code, we find the most innovative solutions to your unique problems, as well as your greatest dreams.</p>
     `,
     about: `
         <h1>About Us</h1>
        <img src="images/service4.PNG" alt="About">
        <img src="images/about.jpg" alt="About Us">
-        <p>Nath Microsystems is a tech firm; which offer a wide range of affordable web design, software development and marketing services. Nath Microsystems develops, design and deploy websites and software applications. Such as: Responsive websites, E - commerce websites, Block chain websites etc. Our services include; development and deployment of software applications, Testing and improving the design of websites, maintaining the appearance of websites by enforcing content standards, designing visual imagery for websites and ensuring that they are in line with branding for clients.
-
-Nath Microsystems is a tech firm established in 2015 and managed by a team of dedicated professionals led by Engr Emenike Nathaniel (Founder/CEO). Over the years, the company has grown to become a high caliber website, Mobile Apps and Business Software Development power-house; serving both local and international clients. Our passion for technology and our expertise in harnessing the plethora of ways by which it can transform business has enable us to deliver incredible customer experiences. Start your next project with us today....A trial will convince you.</p>
+        <p>Nath Microsystems is a tech firm; which offer a wide range of affordable web design, software development and marketing services. We  develops, design and deploy websites and software applications. Such as: Responsive websites, E - commerce websites, Block chain websites etc. Our services include; development and deployment of software applications, Testing and improving the design of websites, maintaining the appearance of websites by enforcing content standards, designing visual imagery for websites and ensuring that they are in line with branding for clients.
+Nath Microsystems was  established in 2015 and managed by a team of dedicated professionals led by Engr Emenike Nathaniel (Founder/CEO). Over the years, the company has grown to become a high caliber website, Mobile Apps and Business Software Development power-house; serving both local and international clients. Our passion for technology and our expertise in harnessing the plethora of ways by which it can transform business has enable us to deliver incredible customer experiences. Start your next project with us today....A trial will convince you.</p>
     `,
     services: `
         <h1>Our Services</h1>
