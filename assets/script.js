47d2498b034ca42d414544d9f5b3e7e7b6c12399
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2025-03-28 22:55:35 +0000

    web upadte

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 const pages = {
     home: `
         <h1>Welcome to Nath Microsystems, your ultimate Web development company in Nigeria</h1>
-        <img src="https://github.com/Githubnath/Nath_Microsystems/blob/main/images/home.jpg" alt="home" width="500">
+        <img src="https://github.com/Githubnath/Nath_Microsystems/blob/main/images/home.jpg" alt="Github image">
         <p>We provide top-notch services to our clients worldwide.</p>
     `,
     about: `
