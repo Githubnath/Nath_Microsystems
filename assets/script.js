fcc9745c388f178e155c76e0597f65ec3e150264
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2025-03-29 21:41:17 +0000

    web update new

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ We thrive in the highly competitive digital landscape by following transparent, 
 <p> We’re more than just a software development company — we’re your digital product development and system integration partner.
 Our focus is to create digital products that generates lasting value for your business. Because we don’t just write code, we find the most innovative solutions to your unique problems, as well as your greatest dreams.</p>
     
-<h2>Get Our High-Performing Web Application Development Solution to Scale Your Business</h2>
+<h3>Get Our High-Performing Web Application Development Solution to Scale Your Business</h3>
 <p>As the top website development company in Nigeria & Africa, we create web solutions that are ready to RACE (Reach, Attract, Communicate, and Engage) your audience. We use the right tech stacks and focus on building fully functional web applications that help you achieve maximum growth. Our end goal is to create quality web apps at a low cost that aligns with your business objectives.</p>
 
 about: `
