7d6fc68c72d34efb7e5bf9bf3bcd6c7d58ffaf55
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2025-04-06 23:45:58 +0000

    updates

--- assets/script.js
+++ assets/script.js
@@ -88,22 +88,6 @@ function createContactPage() {
         </div>`;
 }
 
-const showMoreBtn = document.getElementById('show-more-btn');
-  const moreAbout = document.getElementById('more-about');
-
-  // Add an event listener to the "Learn More" button
-  showMoreBtn.addEventListener('click', function () {
-    // Toggle the visibility of the "more about" section
-    if (moreAbout.classList.contains('hidden')) {
-      moreAbout.classList.remove('hidden');
-      showMoreBtn.textContent = 'Show Less'; // Change button text
-    } else {
-      moreAbout.classList.add('hidden');
-      showMoreBtn.textContent = 'Learn More'; // Change button text back
-    }
-  });
-});
-
 document.addEventListener("submit", (e) => {
     if (e.target.id === "contactForm") {
         e.preventDefault();
