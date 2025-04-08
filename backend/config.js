2a9a06ea9a7bf8ca6a9c5987379b623f5642e718
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2025-04-08 23:08:41 +0000

    back update

--- backend/config.js
+++ backend/config.js
@@ -5,11 +5,13 @@ dotenv.config();
 module.exports = {
   port: process.env.PORT || 5000,
   mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/nathmicrosystems',
-  jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret_here',
+  jwtSecret: process.env.JWT_SECRET || 'K4zq1WkJz2@rTY7pVm9!uFhXs#3LcPwNx$QeZg8TbRmD',
   email: {
     user: process.env.EMAIL_USER || 'your_email@example.com',
     pass: process.env.EMAIL_PASS || 'your_email_password'
   },
   clientUrl: process.env.CLIENT_URL || 'http://localhost:3000'
+};
+'
 };
 
