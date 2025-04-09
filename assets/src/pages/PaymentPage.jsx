c51fd0ef25c2a7b2aa9edf6c932e8e4cfa845a5e
Githubnath <emenikenathaniel55@gmail.com>
2025-04-10 00:28:19 +0100

    new changes

---
+++ assets/src/pages/PaymentPage.jsx
@@ -0,0 +1,10 @@
+import PaymentForm from '../components/PaymentForm';
+export default function PaymentPage() {
+  return (
+    <div>
+      <h2>Make a Payment</h2>
+      <PaymentForm />
+    </div>
+  );
+}
+
