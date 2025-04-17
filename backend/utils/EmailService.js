87f99e9ea506d964c1a4b67155c5b006611189fa
Githubnath <emenikenathaniel55@gmail.com>
2025-04-17 03:23:55 +0100

    add email services

---
+++ backend/utils/EmailService.js
@@ -0,0 +1,16 @@
+// utils/EmailService.js
+
+const nodemailer = require('nodemailer');
+
+const createTransporter = () => {
+  return nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+};
+
+module.exports = createTransporter;
+
